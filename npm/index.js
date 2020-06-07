cb0943e4a778ab264ef0ea182bcc252177a2a9ea
Carlos González <carlos0202@users.noreply.github.com>
2020-06-07 16:32:19 -0400

    added npm packages

--- npm/index.js
+++ npm/index.js
@@ -1,8 +1,8 @@
-var start = new Date(2021, 00, 01);
-var end = new Date(2021, 00, 18);
+const d3 = require('d3-time');
 
-var milisecondsDay = 24 * 60 * 60 * 1000;
+var start = new Date(2021, 02, 01);
+var end = new Date(2021, 03, 01);
 
-var result = (end - start) / milisecondsDay;
+var result = d3.timeDay.count(start, end);
 
 console.log(result);
